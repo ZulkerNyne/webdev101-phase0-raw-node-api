ed4a59e691c37730b145bc77e199caefa64e3667
MD ZULKER NYNE <mdzulkernyne@gmail.com>
2026-01-15 13:46:43 -0700

    phase0: lesson 0.5 routing by path + health + 404

--- src/server.js
+++ src/server.js
@@ -2,10 +2,26 @@ const http =require("http");
 
 const PORT = process.env.PORT? Number(process.env.PORT) : 3000;
 const server = http.createServer((req,res)=>{
-    res.statusCode =200;
-    res.setHeader("Content-Type", "text/plain; charset=utf-8");
-    res.end("Hello from a Node HTTP server!\n");
+    console.log("METHOD:", req.method, "URL:", req.url);
 
+    if (req.url ==="/"){
+        res.statusCode=200;
+        res.setHeader("Content-Type", "text/plain; charset=utf-8");
+        res.end("Home page\n");
+        return;
+    }
+
+    if (req.url ==="/health"){
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json; charset=utf-8");
+        res.end(JSON.stringify({status: "Ok" }));
+        return;
+
+
+    }
+    res.statusCode =404;
+    res.setHeader("Content-Type", "text/plain; charset=utf-8");
+    res.end("NOT FOUND\n");
 });
 
 server.listen(3000,()=>{
